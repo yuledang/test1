fb2b750a59a4c297b6df226a2dd526d907b2c48a
dangyule <515142318@qq.com>
2016-03-23 17:15:03 +0800

    插件

---
+++ jquery插件/谎言/jquery.love-1.0.js
@@ -0,0 +1,68 @@
+﻿/*
+	插件功能：简单表单提交
+	版本：1.0
+	作者：空中幻化
+	参数说明
+		表单本体
+		用户名
+		密码
+		手机号码
+		邮箱
+		提交按钮
+		错误提示语
+*/
+
+
+/*
+	插件功能：程序员撩妹
+	版本：1.0
+	作者：空中幻化
+	参数说明
+		头部字
+		中间字
+		对方
+		自己
+		时间
+		文字出现速度
+*/
+
+;(function($){
+	$.fn.extend({
+		lovely:function(option){
+			var defaults={
+				hearder_s:"",
+				text:"",
+				people:"",
+				"myself":"",
+				"time":"",
+				"speed":"100",
+				heardert:"#header_s",
+				maintxt:"#main_txt",
+				footer:"#foot_ers",
+				way:"#way"
+			},
+			opts=$.extend({},defaults,option);
+			var i=0;
+			$(opts.heardert).append(opts.hearder_s);
+			$(opts.maintxt).append("&nbsp;&nbsp;&nbsp;&nbsp;");
+			$(opts.footer).append(opts.myself+" "+opts.time);
+			$(opts.way).append(opts.people);
+			
+			var lovetxt = opts.text;
+			var lovetext= lovetxt.split("");
+			$.each(lovetext,function(i,n){
+				var str1="<label style='opacity:0'>"+n+"</label>";
+				$(opts.maintxt).append(str1);
+			})
+
+			timer1=setInterval(function(){
+				$(opts.heardert).css("opacity","1");
+				$(opts.maintxt+" "+"label").eq(i).css("opacity","1");
+				i++
+				$(opts.way).css("opacity","1");
+				$(opts.footer).css("opacity","1");
+				
+			},opts.speed)
+		}
+	})
+})(jQuery);
